16ced2df7d50c3e4da2d6ec7bcf963c2add83a99
Qilin Hu <andywhm@163.com>
2025-01-22 09:38:31 +0800

    refactor: 优化强制解包可选类型

--- SwiftSnippets/ViewControllers/02_UIKit/自定义控件/CircularProgressBar/CircularProgressBar.swift
+++ SwiftSnippets/ViewControllers/02_UIKit/自定义控件/CircularProgressBar/CircularProgressBar.swift
@@ -69,7 +69,9 @@ open class CircularProgressBar: UIView {
     private func loadViewFromNib() {
         let bundle = Bundle(for: type(of: self))
         let nib = UINib(nibName: String(describing: type(of: self)), bundle: bundle)
-        let view = nib.instantiate(withOwner: self, options: nil).first as! UIView
+        guard let view = nib.instantiate(withOwner: self, options: nil).first as? UIView else {
+            fatalError("Unable to load view from nib.")
+        }
         view.frame = bounds
         view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         addSubview(view)
