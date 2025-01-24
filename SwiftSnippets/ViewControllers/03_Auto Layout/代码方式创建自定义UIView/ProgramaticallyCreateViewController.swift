062935e1791d0e3a3a4d7aae831a8e226de3f5de
Qilin Hu <andywhm@163.com>
2025-01-24 17:54:29 +0800

    feat: 添加 Auto Layout 使用示例

---
+++ SwiftSnippets/ViewControllers/03_Auto Layout/代码方式创建自定义UIView/ProgramaticallyCreateViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ProgramaticallyCreateViewController.swift
+//  SwiftSnippets
+//
+//  Created by Qilin Hu on 2025/1/24.
+//
+
+import UIKit
+
+/// 代码方式创建自定义 UIView
+/// Reference: <https://medium.com/@tapkain/custom-uiview-in-swift-done-right-ddfe2c3080a>
+class ProgramaticallyCreateViewController: UIViewController {
+    private lazy var addButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("添加自定义视图", for: .normal)
+        button.addTarget(self, action: #selector(addButtonTapped), for: .touchUpInside)
+        return button
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+        navigationItem.largeTitleDisplayMode = .never
+
+        view.addSubview(addButton)
+        NSLayoutConstraint.activate([
+            addButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            addButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+
+    @objc func addButtonTapped(_ sender: UIButton) {
+        let customView = CustomView()
+        customView.translatesAutoresizingMaskIntoConstraints = false
+        customView.layer.borderColor = UIColor.blue.cgColor
+        customView.layer.borderWidth = 2.0
+        view.addSubview(customView)
+
+        NSLayoutConstraint.activate([
+            customView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            customView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+}
