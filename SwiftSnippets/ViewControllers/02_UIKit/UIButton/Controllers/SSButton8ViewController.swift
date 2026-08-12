51e18a2fd8678b3ee6f46b03707ec06b1d40c15a
Hu Qilin <andywhm@163.com>
2026-08-12 14:54:31 +0800

    feat: 添加 UIButton 创建示例代码

--- SwiftSnippets/ViewControllers/02_UIKit/UIButton/Controllers/SSButton8ViewController.swift
+++ SwiftSnippets/ViewControllers/02_UIKit/UIButton/Controllers/SSButton8ViewController.swift
@@ -6,10 +6,14 @@
 //
 
 import UIKit
+import SnapKit
 
 /// 8.提交按钮
 final class SSButton8ViewController: UIViewController {
-    private weak var submitButton: UIButton!
+    private var submitButton: UIButton!
+    private var saveButton: UIButton!
+    
+    var saveButtonTappedAction: (()->Void)?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,18 +29,50 @@ final class SSButton8ViewController: UIViewController {
             make.leading.equalTo(view.snp_leadingMargin)
         }
 
-        // submitButton
-        let submit = UIButton(type: .system)
-        submit.translatesAutoresizingMaskIntoConstraints = false
-        submit.setTitle("提交", for: .normal)
-        submit.addTarget(self, action: #selector(submitButtonTapped), for: .touchUpInside)
-        view.addSubview(submit)
-        submitButton = submit
-
-        NSLayoutConstraint.activate([
-            submit.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            submit.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-        ])
+        // ------------------------------------
+        // submitButton，旧语法
+        submitButton = UIButton(type: .system)
+        submitButton.setTitle("提交", for: .normal)
+        submitButton.addTarget(self, action: #selector(submitButtonTapped), for: .touchUpInside)
+        view.addSubview(submitButton)
+        submitButton.snp.makeConstraints { make in
+            make.trailing.equalTo(view.snp.centerX).offset(-20)
+            make.centerY.equalToSuperview()
+        }
+        
+        // ------------------------------------
+        // saveButton，新语法
+        var config = UIButton.Configuration.plain()
+        config.title = "Save"
+        config.titleAlignment = .center
+        config.baseForegroundColor = UIColor.systemBlue
+        // 设置内容填充边距
+        config.contentInsets = NSDirectionalEdgeInsets(top: 6, leading: 7, bottom: 6, trailing: 7)
+        // 更新字体大小
+        config.titleTextAttributesTransformer = UIConfigurationTextAttributesTransformer({ incoming in
+            var outgoing = incoming
+            outgoing.font = UIFont.systemFont(ofSize: 15, weight: .semibold)
+            return outgoing
+        })
+        saveButton = UIButton(configuration: config, primaryAction: UIAction { [weak self] _ in
+            self?.saveButtonTappedAction?()
+        })
+        // 字体颜色
+        saveButton.configurationUpdateHandler = { button in
+            switch button.state {
+                case .disabled:
+                    button.configuration?.baseForegroundColor = UIColor.systemGray
+                case .highlighted:
+                    button.configuration?.baseForegroundColor = UIColor.systemBlue.darken(by: 0.1)
+                default:
+                    button.configuration?.baseForegroundColor = UIColor.systemBlue
+            }
+        }
+        view.addSubview(saveButton)
+        saveButton.snp.makeConstraints { make in
+            make.leading.equalTo(view.snp.centerX).offset(20)
+            make.centerY.equalToSuperview()
+        }
     }
 
     // MARK: - Actions
